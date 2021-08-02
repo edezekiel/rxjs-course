aec6654594629c33c121553f19ab94a43fddc97e
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-08-02 15:18:19 -0500

    Set the course$ and lessons$ observables in course component

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -22,6 +22,7 @@ import {
 } from "rxjs/operators";
 import { merge, fromEvent, Observable, concat } from "rxjs";
 import { Lesson } from "../model/lesson";
+import { createHttpObservable } from '../common/util';
 
 @Component({
   selector: "course",
@@ -38,6 +39,14 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     const courseId = this.route.snapshot.params["id"];
+
+    this.course$ = createHttpObservable(`/api/courses/${courseId}`);
+
+    this.lessons$ = 
+      createHttpObservable(`/api/lessons?courseId=${courseId}&pageSize=100`)
+        .pipe(
+          map(res => res.payload)
+        )
   }
 
   ngAfterViewInit() {}
