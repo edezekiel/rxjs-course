037b05c5542a34e3b8f283dce6acd8ebcb40853a
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-08-02 14:41:25 -0500

    User merge rxjs operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { concat, fromEvent, interval, of, timer } from 'rxjs';
+import { concat, fromEvent, interval, of, timer, merge } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
@@ -10,12 +11,18 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // merge operator
+    const interval1$ = interval(1000);
+    const interval2$ = interval1$.pipe(map(val => 10 * val));
+    const mergeResult$ = merge(interval1$, interval2$);
+    mergeResult$.pipe(take(10)).subscribe(console.log);
+
     // concat operator
     const source1$ = of([1, 2, 3]);
     const source2$ = of([4, 5, 6]);
     const source3$ = of([7, 8, 9])
     const result$ = concat(source1$, source2$, source3$);
-    result$.subscribe(console.log);
+    // result$.subscribe(console.log);
 
     // Observables
     const interval$ = interval(1000);
