25fffebd352c15e8b1d32b65ef6e434732e4305b
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-08-03 10:52:04 -0500

    Experiment with different types of Subjects

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,6 +9,10 @@ import {
   of,
   timer,
   merge,
+  Subject,
+  BehaviorSubject, 
+  AsyncSubject,
+  ReplaySubject
 } from "rxjs";
 import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
 import { createHttpObservable } from "../common/util";
@@ -19,5 +23,24 @@ import { createHttpObservable } from "../common/util";
   styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-  ngOnInit() {}
+  ngOnInit() {
+    const subject = new ReplaySubject();
+
+    const series1$ = subject.asObservable();
+    const sub1 = series1$.subscribe(val => console.log('sub1:' + val));
+
+    subject.next(1);
+    subject.next(2);    
+    subject.next(3);
+
+    // subject.complete();
+
+    setTimeout(() => {
+      const sub2 = series1$.subscribe(val => console.log('sub2:' + val));
+      subject.next(4);
+      // subject.complete();
+    }, 1000);
+
+    // subject.complete();
+  }
 }
