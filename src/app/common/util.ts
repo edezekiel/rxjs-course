e643b65fe87f6087258ef42969d55e32edd6176e
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-08-03 10:15:19 -0500

    Refactor createHttpObservable function

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,26 +1,17 @@
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
-export function createHttpObservable(url: string) {
-  return Observable.create((observer) => {
+export function createHttpObservable(url: string): Observable<any> {
+  return new Observable(observer => {
     const controller = new AbortController();
     const signal = controller.signal;
 
     fetch(url, { signal })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          observer.error("Request failed with status code: " + response.status);
-        }
-      })
-      .then((body) => {
+      .then(res => res.ok ? res.json() : observer.error('Request failed: ' + res.status))
+      .then(body => {
         observer.next(body);
-
         observer.complete();
       })
-      .catch((err) => {
-        observer.error(err);
-      });
+      .catch(err => observer.error(err));
 
     return () => controller.abort();
   });
